b7be0135aba223348f30ccbda87905ab0011bc81
Pavel Orlovsky <paul.fofanov@gmail.com>
2022-07-04 22:53:54 +0300

    snake kal

--- js/script.js
+++ js/script.js
@@ -9,10 +9,21 @@ const retry = document.getElementById("retry");
 
 const foodIcon = new Image();
 foodIcon.src = "./img/cheese.png";
+const kal = new Image();
+kal.src = "./img/kal.png";
+const dildo = new Image();
+dildo.src = "./img/dildo.png";
+const eatdick = new Audio();
+eatdick.src = "./audio/eatdick.mp3";
+const crushAudio = new Audio();
+crushAudio.src = "./audio/crushAudio.mp3";
 
 let box = 24;
 
 let score = 0;
+
+let kalpovsky = " КАЛПОВСКИЙ".split("");
+console.log(kalpovsky);
 
 let food = {
   x: Math.floor(Math.random() * 15) * box,
@@ -20,6 +31,7 @@ let food = {
 };
 
 let snake = [];
+
 snake[0] = {
   x: 7 * box,
   y: 7 * box,
@@ -62,20 +74,36 @@ function setDirection(e) {
   }
 }
 
+let ms = 300;
+
 function renderApp() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, box * 15, box * 15);
 
-  ctx.drawImage(foodIcon, food.x, food.y);
+  ctx.drawImage(dildo, food.x, food.y);
   for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i === 0 ? "rgb(71, 171, 71)" : "rgb(13, 247, 13)";
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    if (i === 0) {
+      ctx.drawImage(kal, snake[i].x, snake[i].y);
+    } else {
+      ctx.fillStyle = "rgb(255, 190, 229)";
+      ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
   }
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
+  /*   const isCrush = snake.reduce((acc, curr) => {
+    if (acc.x === curr.x && acc.y === curr.y) {
+      return curr;
+    } else {
+      return acc;
+    }
+  });
+  console.log(snake); */
+
   if (snakeX === food.x && snakeY === food.y) {
+    eatdick.play();
     food = {
       x: Math.floor(Math.random() * 15) * box,
       y: Math.floor(Math.random() * 15) * box,
@@ -84,13 +112,39 @@ function renderApp() {
     snake.pop();
   }
 
-  if (snakeX < 0 || snakeX > box * 14 || snakeY < 0 || snakeY > box * 14) {
+  /* if (snakeX < 0 || snakeX > box * 14 || snakeY < 0 || snakeY > box * 14) {
+    clearInterval(refreshFrames);
+  } */
+
+  if (direction === "left") {
+    snakeX -= box;
+  }
+  if (direction === "up") {
+    snakeY -= box;
+  }
+  if (direction === "right") {
+    snakeX += box;
+  }
+  if (direction === "down") {
+    snakeY += box;
+  }
+
+  if (snake.slice(1).some((el) => el.x === snakeX && el.y === snakeY)) {
+    crushAudio.play();
     clearInterval(refreshFrames);
   }
-  if (direction === "left") snakeX -= box;
-  if (direction === "up") snakeY -= box;
-  if (direction === "right") snakeX += box;
-  if (direction === "down") snakeY += box;
+
+  if (snakeX < 0) {
+    snakeX = canvas.width - box;
+  } else if (snakeX >= canvas.width) {
+    snakeX = 0;
+  }
+
+  if (snakeY < 0) {
+    snakeY = canvas.height - box;
+  } else if (snakeY >= canvas.height) {
+    snakeY = 0;
+  }
 
   let updatedSnakeHead = {
     x: snakeX,
@@ -100,7 +154,7 @@ function renderApp() {
   snake.unshift(updatedSnakeHead);
 
   ctx.beginPath();
-  ctx.lineWidth = 2;
+  ctx.lineWidth = 1;
   for (i = 0; i <= 15; i++) {
     ctx.moveTo(0, i * box);
     ctx.lineTo(box * 15, i * box);
@@ -112,4 +166,4 @@ function renderApp() {
   ctx.stroke();
 }
 
-let refreshFrames = setInterval(renderApp, 200);
+let refreshFrames = setInterval(renderApp, ms);
